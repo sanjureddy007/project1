1b0320c2d4f3f3ea02a7f9ed9c441fc8c593d8b5
Sanjay Reddy M <60486088+sanjureddy007@users.noreply.github.com>
2020-05-18 21:25:20 -0400

    Update data.php

--- public/data.php
+++ public/data.php
@@ -1,38 +1,25 @@
 <?php
-function get_rate($name, $rating)
-{     
-  /* Database INFO */
-	$servername = "wyvern.cs.newpaltz.edu";
-	$username = "mudupus1";
-	$password = "u9nd56";   
-    $dbname = "mudupus1_db";
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-       }
 
-       $sql = "SELECT rating FROM movies WHERE title = '$name'";
-       $result = $conn->query($sql);
+function get_rating($name)
+{
+	$titles = [
+		"got"=>7,
+		"breakingbad"=>"7",
+		"truedetective"=>7,
+		"sherlockholmes"=>9,
+		"strangerthings"=>6,
+		"Narcos"=> 8
 
-        if ($result->num_rows > 0) {
-             // output data of each row
-             while($row = $result->fetch_assoc()) {
-                      $p = $row["rating"];
-      }
-    } else {
-                     $p = null;
-        }
-    $conn->close();
-if ($p == $rating) 
-  {
-    return "true";
-  }
-else 
- {
-  return "false";
- }
+	];
+	
+	foreach($titles as $title=>$rating)
+	{
+		if($title==$name)
+		{
+			return $rating;
+			break;
+		}
+	}
 }
 
 ?>
